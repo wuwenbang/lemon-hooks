958f3b382114937aec7e11e0c775c2e8b2216ccd
Pray <64624463+wuwenbang@users.noreply.github.com>
2022-03-23 09:28:41 +0800

    Fix useUpdateEffect hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import { useState } from 'react';
 import './App.css';
 import useAsyncEffect from './hooks/useAsyncEffect';
+import useUpdateEffect from './hooks/useUpdateEffect';
 
 function App() {
-  const [message, setMessage] = useState('');
-  useAsyncEffect(async () => {
-    const res = await new Promise<string>((resolve, reject) => {
-      setTimeout(() => {
-        resolve('hello world!');
-      }, 1000);
-    });
-    setMessage(res);
-  });
-  return <div className="App">Message:{message}</div>;
+  // const [message, setMessage] = useState('');
+  // useAsyncEffect(async () => {
+  //   const res = await new Promise<string>((resolve, reject) => {
+  //     setTimeout(() => {
+  //       resolve('hello world!');
+  //     }, 1000);
+  //   });
+  //   setMessage(res);
+  // });
+  const [count, setCount] = useState(0);
+  useUpdateEffect(() => {
+    console.log('Effect');
+  }, [count]);
+  return (
+    <div>
+      <div> Count:{count}</div>
+      <button onClick={() => setCount(count+1)}>Add Count</button>
+    </div>
+  );
 }
 
 export default App;
--- src/hooks/useUpdateEffect.ts
+++ src/hooks/useUpdateEffect.ts
@@ -2,9 +2,6 @@ import { DependencyList, EffectCallback, useEffect, useRef } from 'react';
 
 export default function useUpdateEffect(effect: EffectCallback, deps?: DependencyList) {
   const isMounted = useRef(false);
-  if (isMounted.current) {
-    useEffect(effect, deps);
-  } else {
-    isMounted.current = true;
-  }
+  useEffect(isMounted.current ? effect : () => {}, deps);
+  if(isMounted.current === false) isMounted.current = true
 }
